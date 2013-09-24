f64746b670ce5c359e4d34b4c244e2f8afe79bb9
Anderson Dadario <anderson@dadario.com.br>
2013-09-23 22:09:40 -0300

    first commit

---
+++ CarangosAppProject/CarangosApp/src/main/java/br/com/caelum/fj59/carangos/infra/MyServer.java
@@ -0,0 +1,27 @@
+package br.com.caelum.fj59.carangos.infra;
+
+import android.os.Build;
+
+public class MyServer {
+    private static String uri;
+
+    static {
+        if (taNoEmulador()) {
+            uri = "http://10.0.2.2:8080/%s";
+        } else {
+			uri = "http://carangos.herokuapp.com/%s";
+//            uri = "http://192.168.84.145:8080/carangos/%s";
+        }
+    }
+
+    public static String uriFor(String value) {
+        return String.format(uri, value);
+    }
+
+    private static boolean taNoEmulador() {
+        return Build.PRODUCT.equals("google_sdk")
+                || Build.PRODUCT.equals("sdk");
+//		return true;
+    }
+
+}
